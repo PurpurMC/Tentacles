4a7171df96171301b70a02b28e8c8ec32a9c8554
mxu-exe <mxu.exe@gmail.com>
2025-07-09 18:30:21 +0100

    Update to 1.21.7

--- scripts/updateUpstream.sh
+++ scripts/updateUpstream.sh
@@ -7,12 +7,12 @@ set -e
 PS1="$"
 
 current=$(cat gradle.properties | grep purpurCommit | sed 's/purpurCommit = //')
-upstream=$(git ls-remote https://github.com/PurpurMC/Purpur | grep ver/1.21.6 | cut -f 1)
+upstream=$(git ls-remote https://github.com/PurpurMC/Purpur | grep ver/1.21.7 | cut -f 1)
 
 if [ "$current" != "$upstream" ]; then
     sed -i 's/purpurCommit = .*/purpurCommit = '"$upstream"'/' gradle.properties
     {
-      ./gradlew applyAllPatches --stacktrace && ./gradlew build --stacktrace && ./gradlew rebuildPurpurPatches --stacktrace && ./gradlew rebuildAllServerPatches --stacktrace
+      ./gradlew applyPurpurSingleFilePatchesFuzzy --stacktrace && ./gradlew rebuildPurpurSingleFilePatches --stacktrace && ./gradlew applyAllPatches --stacktrace && ./gradlew build --stacktrace && ./gradlew rebuildPurpurPatches --stacktrace && ./gradlew rebuildAllServerPatches --stacktrace
     } || exit
 
     git add .
