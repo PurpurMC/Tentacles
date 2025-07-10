1e24ff49bb80b8bda1a6210043a616a91f8a739d
granny <contact@granny.dev>
2025-07-09 17:52:30 -0700

    fuzzy apply single file patches first since any change from upstream likes to break them

--- scripts/updateUpstream.sh
+++ scripts/updateUpstream.sh
@@ -12,7 +12,7 @@ upstream=$(git ls-remote https://github.com/PurpurMC/Purpur | grep ver/1.21.6 | 
 if [ "$current" != "$upstream" ]; then
     sed -i 's/purpurCommit = .*/purpurCommit = '"$upstream"'/' gradle.properties
     {
-      ./gradlew applyAllPatches --stacktrace && ./gradlew build --stacktrace && ./gradlew rebuildPurpurPatches --stacktrace && ./gradlew rebuildAllServerPatches --stacktrace
+      ./gradlew applyPurpurSingleFilePatchesFuzzy --stacktrace && ./gradlew rebuildPurpurSingleFilePatches --stacktrace && ./gradlew applyAllPatches --stacktrace && ./gradlew build --stacktrace && ./gradlew rebuildPurpurPatches --stacktrace && ./gradlew rebuildAllServerPatches --stacktrace
     } || exit
 
     git add .
